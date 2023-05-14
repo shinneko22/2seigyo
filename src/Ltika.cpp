fe2a6c023d2b12a8e1b3aeeefabb7de2c9c779bb
shinneko22 <shinji25@keio.jp>
2023-05-14 17:27:19 +0900

    first commit

---
+++ src/Ltika.cpp
@@ -0,0 +1,14 @@
+/*#include <mbed.h>
+using namespace rtos::ThisThread;
+using ThisThread::sleep_for;
+
+DigitalOut myled(LED1);
+
+int main() {
+    while(1) {
+        myled = 1;
+        sleep_for(250);        
+        myled = 0;
+        sleep_for(250);
+    }
+}*/
